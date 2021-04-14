38555d4ad31b9dba77b6b8e54a650643bbee2489
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-04-15 00:38:35 +0500

    task_test.go add tests

--- 2_task/task_test.go
+++ 2_task/task_test.go
@@ -1,22 +1,43 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPositions(t *testing.T) {
 	pawn := ChessFigure{6, 'e'}
 	knight := ChessFigure{2, 'b'}
-	dfs := DFS{}
-	testChessBoard := makeChessBoard()
+	dfs, testChessBoard := getNewTestObjects()
 	dfs.run(testChessBoard, knight, pawn)
 	expected := "b2 a4 b6 a8 c7 e8 g7 h5 g3 h1 f2 e4 d6 c8 e7 g8 h6 g4 f6 h7 g5 f7 h8 g6 f8 e6"
+	assertEquals(dfs.result, expected, t)
+}
+
+func TestDeltas(t *testing.T) {
+	pawn := ChessFigure{7, 'a'}
+	knight := ChessFigure{3, 'c'}
+	dfs, testChessBoard := getNewTestObjects()
+	dfs.run(testChessBoard, knight, pawn)
+	expected := "c3 b5 a7"
+	assertEquals(dfs.result, expected, t)
+
+	pawn = ChessFigure{7, 'e'}
+	dfs, testChessBoard = getNewTestObjects()
+	dfs.run(testChessBoard, knight, pawn)
+	expected = "c3 b5 a7 c8 e7"
+	assertEquals(dfs.result, expected, t)
+
+	knight = ChessFigure{1, 'h'}
+	dfs, testChessBoard = getNewTestObjects()
+	dfs.run(testChessBoard, knight, pawn)
+	expected = "h1 g3 f5 e7"
 	assertEquals(dfs.result, expected, t)
 }
 
 func TestFromArtem(t *testing.T) {
 	pawn := ChessFigure{5, 'b'}
 	knight := ChessFigure{7, 'e'}
-	testChessBoard := makeChessBoard()
-	dfs := DFS{}
+	dfs, testChessBoard := getNewTestObjects()
 	dfs.run(testChessBoard, knight, pawn)
 	expected := "e7 g8 h6 g4 f6 e8 g7 h5 g3 f5 e7 g6 f8 h7 g5 f7 e5 d7 " +
 		"c5 b7 d8 e6 f4 h3 g1 f3 h4 g2 e1 d3 f2 e4 d6 c8 b6 a8 c7 d5 " +
@@ -36,6 +57,12 @@ func TestWriteRead(t *testing.T) {
 	writeInFile(fileToTest, information)
 	read := readFile(fileToTest)
 	assertEquals(read, information, t)
+}
+
+func getNewTestObjects() (DFS, []map[int32]bool) {
+	testChessBoard := makeChessBoard()
+	dfs := DFS{}
+	return dfs, testChessBoard
 }
 
 func assertEquals(actual string, expected string, test *testing.T) {
