70ebf81a238254aa7904193c922bc930835a27f9
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-11-06 22:06:59 +0500

    5_task refactoring

--- 5_task/src/nativeMain/kotlin/KuhnAlgorithm.kt
+++ 5_task/src/nativeMain/kotlin/KuhnAlgorithm.kt
@@ -1,59 +1,59 @@
-class KuhnAlgorithm(private val T: MutableList<String>, private val matrix: Map<String, List<String>>) {
-    fun run(xDouble: MutableMap<String, String>, yDouble: MutableMap<String, String>): Pair<Boolean, Map<String, String>> {
-        val visited = mutableMapOf<String, MutableList<String>>();
-        for (edge in matrix.keys)
-            visited[edge] = mutableListOf();
-        var indication = 1;
+class KuhnAlgorithm(X: List<String>, private val matrix: Map<String, List<String>>) {
+     private val T: MutableList<String> = X.toMutableList()
+
+    fun run(yDouble: MutableMap<String, String>): Pair<Boolean, Map<String, String>> {
+        val visited = getVisited()
+        var indication = 1
         while (indication != 0 && T.isNotEmpty()) {
-            var x = start();
-            val stack = mutableListOf(x);
-            indication = 0;
+            val stack = mutableListOf(start())
+            indication = 0
             while (stack.isNotEmpty() && indication == 0) {
-                x = stack.last();
-                val y = choice(x, visited);
+                val x = stack.last()
+                val y = choice(x, visited)
                 if (y != "") {
-                    stack.add(y);
-                    val z = yDouble[y]!!;
+                    stack.add(y)
+                    val z = yDouble[y]!!
                     if (z != "") {
-                        stack.add(z);
+                        stack.add(z)
                     }
                     else {
                         indication = 1
                     }
                 }
                 else {
-                    stack.remove(stack.last());
+                    stack.removeLast()
                     if (stack.isNotEmpty())
-                        stack.remove(stack.last());
+                        stack.removeLast()
                 }
             }
             if (indication == 1) {
                 while (stack.isNotEmpty()) {
-                    val y = stack.last();
-                    stack.remove(y);
-                    val x = stack.last();
-                    stack.remove(x);
-                    T.remove(x);
-                    xDouble[x] = y;
-                    yDouble[y] = x;
+                    val y = stack.removeLast()
+                    val x = stack.removeLast()
+                    T.remove(x)
+                    yDouble[y] = x
                 }
             }
         }
         if (indication == 0 || T.isNotEmpty())
-            return Pair(false, xDouble)
-        return Pair(true, xDouble)
+            return Pair(false, yDouble)
+        return Pair(true, yDouble)
+    }
+
+    private fun getVisited(): MutableMap<String, MutableList<String>> {
+        return (matrix.keys).associateWith { mutableListOf<String>() }.toMutableMap()
     }
 
     private fun start(): String {
-        return T[0];
+        return T[0]
     }
 
     private fun choice(x: String, visited: Map<String, MutableList<String>>): String {
         for (edge in matrix[x]!!)
             if (!visited[x]!!.contains(edge)){
-                visited[x]?.add(edge);
+                visited[x]?.add(edge)
                 return edge
             }
-        return "";
+        return ""
     }
 }
--- 5_task/src/nativeMain/kotlin/Main.kt
+++ 5_task/src/nativeMain/kotlin/Main.kt
@@ -1,34 +1,33 @@
 
-fun main(args: Array<String>) {
-    val (matrix, Y) = parse(args[0])
-    val X = matrix.keys.toMutableList()
-    val xDouble = mutableMapOf<String, String>()
-    val yDouble = mutableMapOf<String, String>()
-    for (x in X)
-        xDouble[x] = ""
-    for (y in Y)
-        yDouble[y] = ""
+fun main() {
+    val (matrix, Y) = parse("4\na b c d 0\na b c 0\na b 0\na 0")
+    val X = matrix.keys.toList()
+    val currentMatching = getMatching(Y)
     val alg = KuhnAlgorithm(X, matrix)
-    val (hasMatching, matching) = alg.run(xDouble, yDouble)
+    val (hasMatching, matching) = alg.run(currentMatching)
     if (hasMatching) {
-        println("Y\n${matching.values.joinToString(separator = " ")}")
+        println("Y\n${matching.keys.joinToString(separator = " ")}")
     }
     else {
          println("N")
     }
 }
 
+fun getMatching(edges: List<String>): MutableMap<String, String> {
+    return (edges).associateWith { "" }.toMutableMap()
+}
 
-fun parse(input: String): Pair<Map<String, List<String>>, MutableList<String>> {
+
+fun parse(input: String): Pair<Map<String, List<String>>, List<String>> {
     val text = input.split('\n').iterator()
     val lines = text.next().toInt()
     val matrix = mutableMapOf<String, MutableList<String>>()
     val Y = mutableSetOf<String>()
     (1 until lines + 1).forEach { x ->
-        val elements = text.next().split(' ').dropLast(1);
+        val elements = text.next().split(' ').dropLast(1)
         matrix[x.toString()] = elements.toMutableList()
         Y.addAll(elements)
     }
-    return Pair(matrix, Y.toMutableList())
+    return Pair(matrix, Y.toList())
 }
 
